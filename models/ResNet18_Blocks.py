0f1de97e623fc7695ab85272720ff47383fe4f1d
Adarsh Ghosh <adghosh15@gmail.com>
2025-12-30 21:16:23 +0530

    feat: implement basic block of resnet

---
+++ models/ResNet18_Blocks.py
@@ -0,0 +1,54 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class basicBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, stride=1):
+      super().__init__()
+
+      self.conv1 = nn.Conv2d(
+          in_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=False
+      )
+      self.bn1 = nn.BatchNorm2d(out_channels)
+
+      self.conv2 = nn.Conv2d(
+          out_channels, out_channels, kernel_size=3 , stride = stride, padding=1, bias=False
+          )
+      self.bn2 = nn.BatchNorm2d(out_channels)
+
+      self.shortcut = nn.Identity()
+
+      if stride != 1 or in_channels != out_channels:
+        self.shortcut = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=stride, bias=False),
+            nn.BatchNorm2d(out_channels)
+        )
+
+    def forward(self,x):
+      Identiy=x
+
+      out = self.conv1(x)
+      out = self.bn1(out)
+      out = F.relu(out)
+
+      out = self.conv2(out)
+      out = self.bn2(out)
+
+      out += self.shortcut(x)
+
+      out = F.relu(out)
+
+      return out
+
+      out += self.shortcut(x)
+      out = F.relu(out)
+
+      out = self.conv2(out)
+
+
+x = torch.randn(1, 3, 32, 32)
+
+block = basicBlock(in_channels=3, out_channels=64, stride=1)
+y = block(x)
+
+print("Output shape:", y.shape)
